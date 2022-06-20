e76464bf285e2edb63b2d1bcda851fb7a6ef539c
Lars Andringa <LAndringa@schut.com>
2022-06-20 12:23:18 +0200

    Removed accidental file

--- Test_Projects/validator.py
+++
@@ -1,4 +0,0 @@
-from xsd_validator import XsdValidator
-
-validator = XsdValidator('Thales_Activation/Thales_Activation.xsd')
-validator.assert_valid('Thales_Activation/Examples/ThalesActivation.xml')
